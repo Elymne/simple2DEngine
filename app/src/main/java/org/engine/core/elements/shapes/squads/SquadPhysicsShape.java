3cd5ac1384cf75b518ab41b91fd1c939e4b7dc0e
elymne <sacha.djurdjevic@gmail.com>
2024-09-03 11:09:44 +0200

    refactor : SquadPhysics

--- app/src/main/java/org/engine/core/elements/shapes/squads/SquadPhysicsShape.java
+++ app/src/main/java/org/engine/core/elements/shapes/squads/SquadPhysicsShape.java
@@ -1,16 +1,14 @@
 package org.engine.core.elements.shapes.squads;
 
 import java.util.ArrayList;
-import javax.annotation.Nullable;
 import org.engine.core.attributes.Vector2D;
 import org.engine.core.tools.Geometrie;
 
-// TODO 2 Impulse process.
-// TODO 3 Push process.
 public abstract class SquadPhysicsShape extends SquadCollisionShape {
+    private Vector2D lastPointPosition = new Vector2D(position.getX(), position.getY());
     private Vector2D velocity = new Vector2D(0, 0);
-    private double fallingSpeedCap = 1000;
-    private Vector2D lastPointPosition = new Vector2D(0, 0);
+
+    private double fallingSpeedCap = 1_000;
 
     public SquadPhysicsShape(String name, Vector2D position, double width, double height, int zIndex) {
         super(name, position, width, height, zIndex);
@@ -24,6 +22,14 @@ public abstract class SquadPhysicsShape extends SquadCollisionShape {
         return velocity;
     }
 
+    public double getFallingSpeedCap() {
+        return fallingSpeedCap;
+    }
+
+    public void setFallingSpeedCap(double fallingSpeedCap) {
+        this.fallingSpeedCap = fallingSpeedCap;
+    }
+
     public void push(Vector2D direction, double velocity) {
     }
 
@@ -32,43 +38,54 @@ public abstract class SquadPhysicsShape extends SquadCollisionShape {
 
     @Override
     public void listenCollision(ArrayList<SquadCollisionShape> buffer, int delta) {
-        final SquadCollisionShape ce = getFallingCollisionElement(buffer);
-
-        // No collision check, add falling speed to a certain point.
-        if (ce == null && velocity.getY() > -fallingSpeedCap) {
-            velocity.addY(-(fallingSpeedCap * (delta / 500.0)));
-        }
-
-        // Collision exists, check if it's a Vertical impact.
-        if (ce != null && this.velocity.getY() < 0) {
-            final boolean ccc = Geometrie.detectCollision(
-                    lastPointPosition.getX(),
-                    lastPointPosition.getY() + velocity.getY() * (delta / 1_000.0),
-                    getWidth(),
-                    getHeight(),
-                    ce.getStartPointX(),
-                    ce.getStartPointY(),
-                    getWidth(),
-                    ce.getHeight());
-            System.out.println(ccc);
-
-            velocity.updateY(0);
-            position.addY(ce.getStartPointY() - (getStartPointY() - getHeight()));
-        }
-
-        // Apply velocity.
-        position.add(velocity.getX() * (delta / 1_000.0), velocity.getY() * (delta / 1_000.0));
-        lastPointPosition.updateX(getStartPointX());
-        lastPointPosition.updateY(getStartPointY());
-
-    }
-
-    private @Nullable SquadCollisionShape getFallingCollisionElement(ArrayList<SquadCollisionShape> elements) {
-        for (SquadCollisionShape element : elements) {
-            if (element instanceof SquadStaticShape || element instanceof SquadPhysicsShape) {
-                return element;
+        if (buffer.size() == 0) {
+            if (velocity.getY() > -fallingSpeedCap) {
+                velocity.addY(-(fallingSpeedCap * (delta / 500.0)));
+            }
+        } else {
+            for (SquadCollisionShape ce : buffer) {
+                if (ce instanceof SquadStaticShape || ce instanceof SquadPhysicsShape) {
+                    if (velocity.getY() != 0) {
+                        if (Geometrie.detectCollision(
+                                lastPointPosition.getX(),
+                                lastPointPosition.getY() + velocity.getY() * (delta / 1_000.0),
+                                getWidth(),
+                                getHeight(),
+                                ce.getStartPointX(),
+                                ce.getStartPointY(),
+                                ce.getWidth(),
+                                ce.getHeight())) {
+                            if (velocity.getY() < 0) {
+                                position.addY(ce.getStartPointY() - (getStartPointY() - getHeight()));
+                            } else {
+                                position.addY(ce.getStartPointY() - ce.getHeight() - getStartPointY());
+                            }
+                            velocity.updateY(0);
+                        }
+                    }
+                    if (this.velocity.getX() != 0) {
+                        if (Geometrie.detectCollision(
+                                lastPointPosition.getX() + velocity.getX() * (delta / 1_000.0),
+                                lastPointPosition.getY(),
+                                getWidth(),
+                                getHeight(),
+                                ce.getStartPointX(),
+                                ce.getStartPointY(),
+                                ce.getWidth(),
+                                ce.getHeight())) {
+                            if (velocity.getX() > 0) {
+                                position.addX(ce.getStartPointX() - (getStartPointX() + getWidth()));
+                            } else {
+                                position.addX(ce.getStartPointX() + ce.getWidth() - getStartPointX() - getWidth());
+                            }
+                            velocity.updateX(0);
+                        }
+                    }
+                }
             }
         }
-        return null;
+        lastPointPosition.updateX(getStartPointX());
+        lastPointPosition.updateY(getStartPointY());
+        position.add(velocity.getX() * (delta / 1_000.0), velocity.getY() * (delta / 1_000.0));
     }
 }
--- app/src/main/java/org/engine/presentation/PhysicsSceneTest/PhysicsSceneTest.java
+++ app/src/main/java/org/engine/presentation/PhysicsSceneTest/PhysicsSceneTest.java
@@ -9,25 +9,25 @@ import org.engine.presentation.PhysicsSceneTest.elements.Floor;
 public class PhysicsSceneTest extends Scene {
     private Floor floor = new Floor(0, 0);
     private FallingBlock fallingBlock = new FallingBlock(0, 1200);
-    // private FallingBlock fallingBlock2 = new FallingBlock(10, 400);
-    // private FallingBlock fallingBlock3 = new FallingBlock(200, 600);
-    // private FallingBlock fallingBlock4 = new FallingBlock(20, 1400);
-    // private FallingBlock fallingBlock5 = new FallingBlock(100, 2000);
-    // private FallingBlock fallingBlock6 = new FallingBlock(-200, 2200);
-    // private FallingBlock fallingBlock7 = new FallingBlock(-400, 800);
-    // private FallingBlock fallingBlock8 = new FallingBlock(400, 200);
+    private FallingBlock fallingBlock2 = new FallingBlock(10, 400);
+    private FallingBlock fallingBlock3 = new FallingBlock(200, 600);
+    private FallingBlock fallingBlock4 = new FallingBlock(20, 1400);
+    private FallingBlock fallingBlock5 = new FallingBlock(100, 2000);
+    private FallingBlock fallingBlock6 = new FallingBlock(-200, 2200);
+    private FallingBlock fallingBlock7 = new FallingBlock(-400, 800);
+    private FallingBlock fallingBlock8 = new FallingBlock(400, 200);
 
     public PhysicsSceneTest() {
         super("Physics scene");
         CameraRuler.getInstance().setFocus(new Vector2D(0, 200));
         this.addElement(floor);
         this.addElement(fallingBlock);
-        // this.addElement(fallingBlock2);
-        // this.addElement(fallingBlock3);
-        // this.addElement(fallingBlock4);
-        // this.addElement(fallingBlock5);
-        // this.addElement(fallingBlock6);
-        // this.addElement(fallingBlock7);
-        // this.addElement(fallingBlock8);
+        this.addElement(fallingBlock2);
+        this.addElement(fallingBlock3);
+        this.addElement(fallingBlock4);
+        this.addElement(fallingBlock5);
+        this.addElement(fallingBlock6);
+        this.addElement(fallingBlock7);
+        this.addElement(fallingBlock8);
     }
 }
